a7d4d4ef26d00ffd75abd8bc1e4d90583f07c7cb
Christoph Hellwig <hch@lst.de>
2017-04-20 16:02:54 +0200

    pd: don't check blk_execute_rq return value.
    The driver never sets req->errors, so blk_execute_rq will always return 0.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Bart Van Assche <Bart.VanAssche@sandisk.com>
    Reviewed-by: Johannes Thumshirn <jthumshirn@suse.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>

--- drivers/block/paride/pd.c
+++ drivers/block/paride/pd.c
@@ -721,7 +721,6 @@ static int pd_special_command(struct pd_unit *disk,
 		      enum action (*func)(struct pd_unit *disk))
 {
 	struct request *rq;
-	int err = 0;
 
 	rq = blk_get_request(disk->gd->queue, READ, __GFP_RECLAIM);
 	if (IS_ERR(rq))
@@ -729,11 +728,9 @@ static int pd_special_command(struct pd_unit *disk,
 
 	rq->cmd_type = REQ_TYPE_DRV_PRIV;
 	rq->special = func;
-
-	err = blk_execute_rq(disk->gd->queue, disk->gd, rq, 0);
-
+	blk_execute_rq(disk->gd->queue, disk->gd, rq, 0);
 	blk_put_request(rq);
-	return err;
+	return 0;
 }
 
 /* kernel glue structures */
