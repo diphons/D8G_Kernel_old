d42e083afc97c75033d626562e805cc68d96ff96
diphons <diphons@gmail.com>
2023-05-28 19:39:01 +0700

    Patch upstream linux 4.9.320 - accidentaly skipped Need this to add support LRNG this upstream will get stuck for entropy RNG.. use patch revert old random to fix entropy RNG

--- include/uapi/linux/swab.h
+++ include/uapi/linux/swab.h
@@ -133,6 +133,15 @@ static inline __attribute_const__ __u32 __fswahb32(__u32 val)
 	__fswab64(x))
 #endif
 
+static __always_inline unsigned long __swab(const unsigned long y)
+{
+#if BITS_PER_LONG == 64
+	return __swab64(y);
+#else /* BITS_PER_LONG == 32 */
+	return __swab32(y);
+#endif
+}
+
 /**
  * __swahw32 - return a word-swapped 32-bit value
  * @x: value to wordswap
